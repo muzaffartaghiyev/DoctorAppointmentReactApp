616fd6a3d8ec87b92f231dee699de48f935b2874
muzeffertagiyev <muzaffar.taghiyev@gmail.com>
2025-05-15 15:40:22 +0300

    home page was updated

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react'
 
-import {Container} from "react-bootstrap"
+import {Container,Image} from "react-bootstrap"
 
 import Doctors from "../components/Doctors"
 import AppointmentList from "../components/AppointmentList"
@@ -15,7 +15,8 @@ const Home = () => {
     <Container className='d-flex flex-column justify-content-center align-items-center'>
         <h1 className='display-4 fw-normal text-danger mt-3'>CLARUS HOSPITAL</h1>
         <Doctors doctorData={doctorData} appointmentList={appointmentList} setAppointmentList={setAppointmentList}/>
-        <AppointmentList appointmentList={appointmentList} setAppointmentList={setAppointmentList}/>
+        {appointmentList.length===0?(<Image src='./img/appointment.jpg' className='w-50 m-5' rounded   />) : (<AppointmentList appointmentList={appointmentList} setAppointmentList={setAppointmentList}/>)}
+        
     </Container>
   )
 }
